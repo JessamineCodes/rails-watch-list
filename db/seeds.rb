310f7c017bd60861726b774484c508fdf161de55
Jessamine McHugh <jessamine.mchugh@gmail.com>
2023-02-23 17:08:02 +0000

    watch list ex comp wo styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+data = JSON.parse(response)
+movies_array = data["results"]
+img_url = "https://image.tmdb.org/t/p/w500"
+
+movies_array.each do |movie|
+  new_movie = Movie.new(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: img_url+movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+  new_movie.save
+end
